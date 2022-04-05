d98bd1ab721826070e81f618113f75ac7e1d97de
fuhao1px <fuhao1px@163.com>
2022-04-05 23:05:53 +0800

    init

---
+++ packages/Button/lib/Button.ts
@@ -0,0 +1,5 @@
+import { withInstall } from '@lerna1/utils';
+
+const res = withInstall();
+
+export { res };
---
+++ packages/Text/lib/Text.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = text;
+
+function text() {
+    // TODO 123123123
+}
