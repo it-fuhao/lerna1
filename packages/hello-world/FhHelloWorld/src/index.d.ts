a9e732e60457eea66dc352a35cf367bec905aabb
fuhao1px <fuhao1px@163.com>
2022-04-07 21:56:19 +0800

    init

--- packages/hello-world/FhHelloWorld/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const propsTypes: {
-    name: StringConstructor;
-};
--- packages/hello-world/FhHelloWorld/src/index.vue.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: import("vue").DefineComponent<{}, {
-    msg: import("vue").Ref<number>;
-}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, import("vue").EmitsOptions, string, import("vue").VNodeProps & import("vue").AllowedComponentProps & import("vue").ComponentCustomProps, Readonly<import("vue").ExtractPropTypes<{}>>, {}>;
-export default _default;
