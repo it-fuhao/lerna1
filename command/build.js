e2370f67c38de4b8877c818cce2b2234ade41a42
fuhao <fuhao1px@163.com>
2022-04-08 11:18:22 +0800

    init

--- command/build.js
+++ command/build.js
@@ -1,27 +1,30 @@
 
-// import path from "path";
-// import { defineConfig, build } from "vite";
-// import vue from "@vitejs/plugin-vue";
-// import vueJsx from "@vitejs/plugin-vue-jsx";
-// import dts from "vite-plugin-dts";
-const path = require("path");
-const { defineConfig, build } = require("vite");
-const vue = require("@vitejs/plugin-vue");
-const vueJsx = require("@vitejs/plugin-vue-jsx")
-const dts = require("vite-plugin-dts")
+
+import fs from "fs-extra";
+import path from "path";
+import chalk from "chalk";
+import symbols from "log-symbols";
+import { defineConfig, build } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import dts from "vite-plugin-dts";
 
 const outputDir = "dist"; // 构建产物文件夹名称
+// const __dirname = path.resolve();
 
 // 打包的入口文件
-console.log(1, __dirname);
-console.log(path.resolve(__dirname));
-console.log(path.resolve(__dirname, '../packages'));
+// console.log(1, __dirname);
+// console.log(path.resolve(__dirname));
+// console.log(path.resolve('./packages'));
+// console.log(path.resolve(__dirname, './packages'));
 console.log(process.argv);
+console.log(process.cwd());
+console.log(path.resolve("./"));
 const packageName = process.argv[2].split("=")[1];
-const entryDir = path.resolve(__dirname, `../packages/${packageName}`);
+const entryDir = path.resolve("./");
 console.log(entryDir);
 // 出口文件夹
-const outDir = path.resolve(__dirname, `../packages/${packageName}/${outputDir}`);
+const outDir = path.resolve(`./${outputDir}`);
 console.log("outDir", outDir);
 // vite基础配置
 const baseConfig = defineConfig({
@@ -65,7 +68,41 @@ const buildAll = async () => {
     },
   })
 }
-const buildComponent = async () => {
-  await buildAll()
+
+/**
+ * 删除文件夹
+ * @param {*} dir 需要删除的文件夹 
+ * @returns 
+ */
+const removeDir = dir => {
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(dir)) {
+      //先读文件夹
+      fs.stat(dir, (err, stat) => {
+        if (stat.isDirectory()) {
+          fs.readdir(dir, (err, files) => {
+            files = files.map(file => path.join(dir, file));
+            files = files.map(file => removeDir(file));
+            Promise.all(files).then(() => {
+              fs.rmdir(dir, resolve);
+            })
+          })
+        }else {
+          fs.unlink(dir, resolve);
+        }
+      })
+    } else {
+      resolve(true);
+    }
+  })
 }
-buildComponent()
+
+const buildComponent = async () => {
+  // 先删除dist
+  await removeDir(outDir);
+  // 开始构建
+  await buildAll();
+  console.log(symbols.success, chalk.green(`${packageName}构建成功！`));
+}
+
+buildComponent();
