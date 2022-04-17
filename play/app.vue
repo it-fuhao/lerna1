e28e00c901db2d55e6e43221f003d2b413334a7f
fuhao1px <fuhao1px@163.com>
2022-04-17 20:28:56 +0800

    init

--- play/app.vue
+++ play/app.vue
@@ -1,25 +1,29 @@
 <template>
   <p>123</p>
   <FhHelloWorld></FhHelloWorld>
-  <!-- <van-button>annual</van-button> -->
+  <Button>annual</Button>
 </template>
 <script lang="ts">
-// import { defineComponent } from 'vue'
-// // import { Button, Collapse } from 'vant';
+import { defineComponent } from 'vue'
+// import { Button, Collapse } from 'vant';
 // import FhHelloWorld from '../packages/hello-world';
-// export default defineComponent({
-//   components: {
-//     // [Button.name]: Button,
-//     // 'vant-button': Button,
-//     FhHelloWorld
-//   },
-//   setup() {
+import FhHelloWorld from '@lerna1/hello-world';
+export default defineComponent({
+  components: {
+    // [Button.name]: Button,
+    // 'vant-button': Button,
+    FhHelloWorld
+  },
+  setup() {
     
-//   },
-// })
+  },
+})
 </script>
 <script lang="ts" setup>
-import FhHelloWorld from '../packages/hello-world';
+// import FhHelloWorld from '../packages/hello-world';
+// import { Button, Collapse } from 'vant';
+// import FhHelloWorld from '@lerna1/hello-world';
+
 </script>
 
 <style scoped>
--- play/vite.config.ts
+++ play/vite.config.ts
@@ -1,12 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+// import styleImport from 'vite-plugin-style-import';
 import styleImport, { VantResolve } from 'vite-plugin-style-import';
 
 export default defineConfig({
   plugins: [
     vue(),
+    // styleImport({
+    //   libs: [
+    //     {
+    //       libraryName: "vant",
+    //       esModule: true,
+    //       resolveStyle: name => `vant/es/${name}/style`,
+    //     },
+    //   ],
+    // }),
     styleImport({
-      resolves: [VantResolve()],
+      resolves: [
+        VantResolve(), 
+        {
+          libraryName: "lerna1",
+          ensureStyleFile: true
+        }
+      ],
     }),
   ],
   publicDir: '/'
